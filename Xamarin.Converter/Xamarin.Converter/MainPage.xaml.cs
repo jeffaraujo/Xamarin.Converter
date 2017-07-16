937e2ec7ea5b0ee0c58c7c8afa61a5ed28a3bf84
jeffa <jeffa@JEFERSON-LAPTOP>
2017-07-16 18:59:12 -0300

    Add project files.

---
+++ Xamarin.Converter/Xamarin.Converter/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin.Converter
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnConverter.Clicked += BtnConverter_Clicked;
+        }
+
+        private void BtnConverter_Clicked(object sender, EventArgs e)
+        {
+            if(txtValorOrigem.Text == null) return;
+            
+
+            double valor1 = Convert.ToDouble(txtValorOrigem.Text);
+
+            txtValorConvertido.Text = "US$ " + (Math.Round( valor1 / 3.20, 2)).ToString();
+
+        }
+    }
+}
